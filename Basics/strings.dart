0a5385c5cfce38c0ff29d47af670e06bb08b94ef
Kashif <ahmed.kashif1001@gmail.com>
2024-09-03 14:55:18 +0500

    strings and string interpolation

---
+++ Basics/strings.dart
@@ -0,0 +1,7 @@
+void main(){
+  var name = "Mario";
+  print(name);
+
+//string interpolation
+  print("My name is $name");
+}
--- Basics/variables.dart
+++ Basics/variables.dart
@@ -14,5 +14,6 @@ void main(){
 
   //But what's the difference between the two and when to use either of them?
 
+  //When we use the final keyword variable then it becomes a runtime constant and when we create a variable with const keyword then the variable is a compile time constant. So, we use final when we don't know what a value will be at the compile time. For example, if we need to fetch some data and store it in a variale or if the value of a variable depends on the return value of some other functions then we might use final, we will use const when we do know the value of a variable at compile time when things are there since the beginning.
 
 }
