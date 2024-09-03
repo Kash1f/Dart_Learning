1b60c028b9a33d216616e1a96c36158fbb6cfcb7
Kashif <ahmed.kashif1001@gmail.com>
2024-09-03 13:36:41 +0500

    final const variables

--- Basics/main.dart
+++ Basics/main.dart
@@ -1,4 +1,9 @@
-void main(){
-    print("Hello World");
-}
+// void main(){
+//     print("Hello World");
+// }
 
+void main(){
+  for(var i = 0; i <= 5; i++){
+    print('Hello $i');
+  }
+}
--- Basics/variables.dart
+++ Basics/variables.dart
@@ -1,4 +1,18 @@
 void main(){
   var name = "Batman";
+
+  // name = 15; //cannot re-assign an integer or anything else, only string can be re-assigned
+  name = "Superman";
   print(name);
+
+  //The value of final and const variables cannot be changed
+  final age = 25;
+  // age = 30;
+
+  const isOpen = true;
+  // isOpen = false;
+
+  //But what's the difference between the two and when to use either of them?
+
+
 }
