1f64b23bf73b88e5e24197b2fb675fd836cbf04c
Kashif <ahmed.kashif1001@gmail.com>
2024-09-03 16:49:30 +0500

    dynamic and non dynamic lists

--- Lists and Sets/lists.dart
+++ Lists and Sets/lists.dart
@@ -1,4 +1,16 @@
 void main(){
   var a = [1,2,3,4];
   print(a);
+
+  //Mixed list:
+  var scoresAndnames = [40, "mario", 60 , "shawn", 80, "john"];
+  print(scoresAndnames);
 }
+
+// void main(){
+
+//   //This list can only store integers
+//   List<int> scores = [66, 74, 89, 58, 86, 94];
+
+// }
+
