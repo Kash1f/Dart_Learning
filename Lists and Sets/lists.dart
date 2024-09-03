6a3236c1cf2b430f46cf4fa71c52f1bf302fdaad
Kashif <ahmed.kashif1001@gmail.com>
2024-09-03 17:14:56 +0500

    list(array) methods, add remove, removeLast

--- Lists and Sets/lists.dart
+++ Lists and Sets/lists.dart
@@ -1,16 +1,24 @@
-void main(){
-  var a = [1,2,3,4];
-  print(a);
-
-  //Mixed list:
-  var scoresAndnames = [40, "mario", 60 , "shawn", 80, "john"];
-  print(scoresAndnames);
-}
-
 // void main(){
+//   var a = [1,2,3,4];
+//   print(a);
 
-//   //This list can only store integers
-//   List<int> scores = [66, 74, 89, 58, 86, 94];
-
+//   //Mixed list:
+//   var scoresAndnames = [40, "mario", 60 , "shawn", 80, "john"];
+//   print(scoresAndnames);
 // }
+
+void main(){
+
+  //This list can only store integers
+  List<int> scores = [66, 74, 89, 58, 86, 94];
+
+  scores[1] = 84;
+  scores.add(100);
+  scores.remove(58);
+  scores.removeLast();
+
+
+  print(scores);
+
+}
 
