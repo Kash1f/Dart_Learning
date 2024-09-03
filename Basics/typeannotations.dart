a7d03520577a847c2fdd7ca27ceb70fd8d3da1b8
Kashif <ahmed.kashif1001@gmail.com>
2024-09-03 15:20:35 +0500

    data types, int bool double

--- Basics/typeannotations.dart
+++ Basics/typeannotations.dart
@@ -1,4 +1,15 @@
 void main(){
   String name = "Mario";
   print(name);
+
+  int age = 40;
+  print(age);
+  
+  bool isTrue = true; 
+  print(isTrue);
+  
+  double averagePrice = 55.5;
+  print(averagePrice);
+  
+  
 }
