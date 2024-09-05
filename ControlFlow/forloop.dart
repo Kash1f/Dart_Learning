ba55a1eb5dc7017e88b82d953af6feb61bbc69d5
Kashif <ahmed.kashif1001@gmail.com>
2024-09-05 16:50:24 +0500

    maps, empty map

--- ControlFlow/forloop.dart
+++ ControlFlow/forloop.dart
@@ -1,8 +1,21 @@
 void main() {
-  List<int> scores = [66, 74, 89, 58, 86, 94];
+  List<int> scores = [20, 74, 89, 10, 86, 94];
 
-  for(int i = 0; i <= 5; i++){
-    print(scores[i]);
+  //variable i will iterate over the above list of integers
+  // for(int i = 0; i <= 5; i++){
+  //   print(scores[i]);
+  // }
+
+  //Accessing each list value using for-each
+  for(int score in scores){
+    if(score > 50){
+    print("The score is $score");
+  } 
   }
+
+  //We want to loop through the list but where a certain condition is met
+  for(int score in scores.where((s)=> s>50)){
+    print ("The score is $score");
+}
 
 }
---
+++ Maps/maps.dart
@@ -0,0 +1,6 @@
+void main(){
+
+
+  //empty map
+   var planets = {};
+}
